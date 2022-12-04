f72066562e3e15ac8396b6065af480bf91694277
niekz <johannventer2@Gmail.com>
2022-12-04 07:44:30 +0200

    Added day4

---
+++ day4/day4-2.py
@@ -0,0 +1,19 @@
+with open('day4-1.txt') as f:
+    lines = f.readlines()
+
+total = 0
+
+for grouping in lines:
+  assignments = grouping.strip().split(",")
+  
+  ass1_ranges = assignments[0].split("-")
+  ass2_ranges = assignments[1].split("-")
+  
+  ass1 = [i for i in range(int(ass1_ranges[0]), int(ass1_ranges[1])+1)]
+  ass2 = [i for i in range(int(ass2_ranges[0]), int(ass2_ranges[1])+1)]
+
+  if any(item in ass1 for item in ass2) or any(item in ass2 for item in ass1):
+    total = total + 1
+
+print(total)
+  
