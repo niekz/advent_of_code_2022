ab05fcc1e20b777c3882be8f700ee82e9c903573
niekz <johannventer2@Gmail.com>
2022-12-06 07:19:27 +0200

    Day 6

---
+++ day6/day6-1.py
@@ -0,0 +1,11 @@
+with open('day6-1.txt') as f:
+    lines = f.readlines()
+
+seq = [character for character in lines[0]]
+window_size = 4
+
+for i in range(len(seq) - window_size + 1):
+    currentSeq = seq[i: i + window_size]
+    if len(currentSeq) == len(set(currentSeq)):
+        print("First unique indicator: " + str(i+window_size))
+        break
